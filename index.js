40628c4eb2b5a5a5cd8966fe28e2c6f37ef09293
danni-nu <145931898+danni-nu@users.noreply.github.com>
2025-03-10 20:46:30 -0400

    Revert "Feature04new33"

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Sequelize, DataTypes } = require('sequelize');
 
 const app = express();
 app.use(express.json());
-const PORT = process.env.NODE_PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.text({ type: '*/*' }));
 app.use(express.urlencoded({ extended: true }));
@@ -85,8 +85,8 @@ app.all('/healthz', (req, res) => {
 });
 
 if (require.main === module) {
-    app.listen(PORT, "0.0.0.0", () => {
-        console.log(`Server running on http://0.0.0.0:${PORT}`);
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
     });
 }
 
