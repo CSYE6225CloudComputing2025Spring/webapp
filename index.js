73051119d0bf067c4b2eca3ea2b1cf0770df967d
danni-nu <zhou.dann@northeastern.edu>
2025-03-10 21:09:59 -0400

    change to 0.0.0.0

--- index.js
+++ index.js
@@ -85,9 +85,9 @@ app.all('/healthz', (req, res) => {
 });
 
 if (require.main === module) {
-    app.listen(PORT, () => {
-        console.log(`Server running on http://localhost:${PORT}`);
-    });
+    app.listen(PORT, "0.0.0.0", () => {
+        console.log(`Server running on http://0.0.0.0:${PORT}`);
+    });    
 }
 
 module.exports = app;
